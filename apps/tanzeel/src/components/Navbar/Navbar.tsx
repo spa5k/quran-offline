ad6e20521298074f8d674d00a624453e25c2023f
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-03 21:57:35 +0530

    feat(navbar): ✨ initial navbar implementationd done

--- apps/tanzeel/src/components/Navbar/Navbar.tsx
+++ apps/tanzeel/src/components/Navbar/Navbar.tsx
@@ -1,48 +1,131 @@
 import {
-  Heading,
+  Box,
+  Button,
+  chakra,
+  CloseButton,
+  Flex,
   HStack,
   IconButton,
   Input,
   InputGroup,
   InputLeftElement,
-  Link,
   useColorMode,
+  useColorModeValue,
+  useDisclosure,
+  VisuallyHidden,
+  VStack,
 } from '@chakra-ui/react';
-// import { ReactComponent as Logo } from '../../images//Logo.svg';
+import React from 'react';
 import Logo from '~icons/brand/logo';
 import IconSun from '~icons/clarity/sun-line';
+import IconMenu from '~icons/eva/menu-outline';
+import IconHeadphone from '~icons/fluent/headphones-48-regular';
 import IconSearch from '~icons/mdi/search';
 import IconNight from '~icons/mdi/weather-night';
 
 export const NavBar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue('white', 'gray.800');
+  const mobileNav = useDisclosure();
 
   return (
-    <HStack spacing={50} width="full" justifyContent="space-between">
-      <HStack>
-        <Logo height={100} width="auto" />
-        <Link>
-          <Heading>Quran</Heading>
-        </Link>
-        <Link>
-          <Heading>Bookmarks</Heading>
-        </Link>
-      </HStack>
-      <HStack>
-        <InputGroup>
-          <InputLeftElement>
-            <IconSearch />
-          </InputLeftElement>
-          <Input placeholder="Search" width={300} />
-        </InputGroup>
-        <IconButton
-          icon={colorMode === 'light' ? <IconSun /> : <IconNight />}
-          aria-label="nice"
-          onClick={toggleColorMode}
-          variant="solid"
-          size="lg"
-        />
-      </HStack>
-    </HStack>
+    <React.Fragment>
+      <chakra.header
+        bg={bg}
+        w="full"
+        px={{ base: 2, sm: 4 }}
+        py={4}
+        shadow="md"
+      >
+        <Flex alignItems="center" justifyContent="space-between" mx="auto">
+          <HStack display="flex" spacing={3} alignItems="center">
+            <Box display={{ base: 'inline-flex', md: 'none' }}>
+              <IconButton
+                display={{ base: 'flex', md: 'none' }}
+                aria-label="Open menu"
+                fontSize="20px"
+                color={useColorModeValue('gray.800', 'inherit')}
+                variant="ghost"
+                icon={<IconMenu />}
+                onClick={mobileNav.onOpen}
+              />
+              <VStack
+                pos="absolute"
+                top={0}
+                left={0}
+                right={0}
+                display={mobileNav.isOpen ? 'flex' : 'none'}
+                flexDirection="column"
+                p={2}
+                pb={4}
+                m={2}
+                bg={bg}
+                spacing={3}
+                rounded="sm"
+                shadow="sm"
+              >
+                <CloseButton
+                  aria-label="Close menu"
+                  justifySelf="self-start"
+                  onClick={mobileNav.onClose}
+                />
+                <Button w="full" variant="ghost">
+                  Quran
+                </Button>
+                <Button w="full" variant="ghost">
+                  Bookmarks
+                </Button>
+              </VStack>
+            </Box>
+            <chakra.a
+              href="/"
+              title="Choc Home Page"
+              display="flex"
+              alignItems="center"
+            >
+              <Logo height={50} width="auto" />
+
+              <VisuallyHidden>Choc</VisuallyHidden>
+            </chakra.a>
+
+            <HStack spacing={3} display={{ base: 'none', md: 'inline-flex' }}>
+              <Button variant="ghost" size="sm">
+                Quran
+              </Button>
+              <Button variant="ghost" size="sm">
+                Bookmarks
+              </Button>
+            </HStack>
+          </HStack>
+          <HStack
+            spacing={3}
+            display={mobileNav.isOpen ? 'none' : 'flex'}
+            alignItems="center"
+          >
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<IconSearch />}
+              />
+              <Input type="tel" placeholder="Search..." />
+            </InputGroup>
+            <IconButton
+              icon={colorMode === 'light' ? <IconSun /> : <IconNight />}
+              aria-label="nice"
+              onClick={toggleColorMode}
+              variant="solid"
+              size="lg"
+            />
+            <IconButton
+              icon={<IconHeadphone />}
+              aria-label="headphones"
+              variant="solid"
+              size="lg"
+            />
+            IconHeadphone
+          </HStack>
+        </Flex>
+      </chakra.header>
+    </React.Fragment>
   );
 };
