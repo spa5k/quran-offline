d170b6c62ff90599aab99fe4392a969652ae2561
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-06 19:50:54 +0530

    feat(data): ✨ added a way to dynamically download quran data in json format

--- apps/tanzeel/src/components/Middle/ChaptersGrid.tsx
+++ apps/tanzeel/src/components/Middle/ChaptersGrid.tsx
@@ -3,6 +3,7 @@ import { ChapterBox } from '../Chapter';
 
 export const ChaptersGrid = () => {
 	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8];
+
 	return (
 		<Wrap spacing='30px'>
 			{arr.map((_, index) => <ChapterBox number={index + 1} />)}
