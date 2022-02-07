1a18ae9229a64299820daa0757d10abe6c968d23
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-07 20:14:14 +0530

    feat(data): ✨ added initial usage of data into the chapters

--- apps/tanzeel/src/components/Middle/ChaptersGrid.tsx
+++ apps/tanzeel/src/components/Middle/ChaptersGrid.tsx
@@ -1,12 +1,36 @@
 import { Wrap } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { getChapterList } from '../../utils/getChapterList';
+import { Chapter } from '../../utils/type';
 import { ChapterBox } from '../Chapter';
 
 export const ChaptersGrid = () => {
-	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8];
+	const [data, setData] = useState<Chapter[]>([]);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		getChapterList()
+			.then(response => {
+				setData(response);
+				setLoading(false);
+			});
+	}, []);
+
+	if (loading && !data) {
+		return <Wrap>Loading...</Wrap>;
+	}
 
 	return (
 		<Wrap spacing='30px'>
-			{arr.map((_, index) => <ChapterBox number={index + 1} />)}
+			{data.map((chapter, index) => (
+				<ChapterBox
+					id={chapter.id}
+					name={chapter.name}
+					total_verses={chapter.total_verses}
+					translation={chapter.translation}
+					transliteration={chapter.transliteration}
+					type={chapter.type}
+				/>
+			))}
 		</Wrap>
 	);
 };
