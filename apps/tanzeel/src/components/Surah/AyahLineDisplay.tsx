af4867c1b172c733fa5245bb280965a8aea6bab4
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-15 20:44:10 +0530

    feat(fonts): 💄 added basic font changer to surah page

--- apps/tanzeel/src/components/Surah/AyahLineDisplay.tsx
+++ apps/tanzeel/src/components/Surah/AyahLineDisplay.tsx
@@ -1,11 +1,16 @@
 import { chakra, Text, WrapItem } from '@chakra-ui/react';
+import { useAtom } from 'jotai';
+import { changeFontAtom } from '../../state/changeFontAtom';
 import { Lafz } from '../../utils/types';
 
-export const AyahLineDisplay = ({ ayah }: { ayah: Lafz; }): JSX.Element => (
-	<WrapItem flexDir='column' alignItems='center'>
-		<Text fontSize='xxx-large' fontFamily='IndoPak'>
-			{ayah.textIndopak}
-		</Text>
-		<chakra.span>{ayah.translation}</chakra.span>
-	</WrapItem>
-);
+export const AyahLineDisplay = ({ ayah }: { ayah: Lafz; }): JSX.Element => {
+	const [font] = useAtom(changeFontAtom);
+	return (
+		<WrapItem flexDir='column' alignItems='center'>
+			<Text fontSize='xxx-large' fontFamily={font}>
+				{ayah.textIndopak}
+			</Text>
+			<chakra.span>{ayah.translation}</chakra.span>
+		</WrapItem>
+	);
+};
---
+++ apps/tanzeel/src/components/Surah/Font/FontChanger.tsx
@@ -0,0 +1,22 @@
+import { Button, ButtonGroup, Flex } from '@chakra-ui/react';
+import { useAtom } from 'jotai';
+import { changeFontAtom } from '../../../state/changeFontAtom';
+enum TypesofFonts {
+	Uthmani = 'UthmanicHafs',
+	IndoPak = 'IndoPak',
+}
+
+export const FontChanger = (): JSX.Element => {
+	const [font, setFont] = useAtom(changeFontAtom);
+	return (
+		<Flex flexDir='column' alignItems='center'>
+			<ButtonGroup>
+				<Button onClick={() => setFont(TypesofFonts.Uthmani)}>Uthmani</Button>
+
+				<Button onClick={() => setFont(TypesofFonts.IndoPak)}>IndoPak</Button>
+			</ButtonGroup>
+
+			<p>Current font - {font}</p>
+		</Flex>
+	);
+};
