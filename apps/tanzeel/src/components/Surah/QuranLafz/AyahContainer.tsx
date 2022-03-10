0eab85f5f387c6a391323a0573db34228f6630ee
Spark <79936503+spa5k@users.noreply.github.com>
2022-03-10 20:59:40 +0530

    feat(recitation): ✨ added recitation through webview

--- apps/tanzeel/src/components/Surah/QuranLafz/AyahContainer.tsx
+++ apps/tanzeel/src/components/Surah/QuranLafz/AyahContainer.tsx
@@ -1,21 +1,34 @@
-import { Button, Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverTrigger, Wrap, WrapItem } from '@chakra-ui/react';
+import { Button, ButtonGroup, Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverTrigger, Wrap, WrapItem } from '@chakra-ui/react';
 import { useAtom } from 'jotai';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAudio } from 'react-use';
 import { ayahPlayerAtom } from '../../../state/ayahPlayerAtom';
-import { playOneAyah } from '../../../utils/playOneAyah';
+import { getAyahRecitationUrl } from '../../../utils/getAyahRecitationUrl';
 import { Lafz } from '../../../utils/types';
 import { LafzDisplay } from './LafzDisplay';
 
-// memoize this components
-
 export const AyahContainer = React.memo(({ lafz, ayahNumber, surahNumber }: { lafz: Lafz[]; ayahNumber: number; surahNumber: number; }): JSX.Element => {
 	const [, setAyahStatus] = useAtom(ayahPlayerAtom);
+	const [ayahUrl, setAyahUrl] = useState<string>('');
+	const [audio, state, controls, ref] = useAudio({
+		src: ayahUrl ? ayahUrl : '',
+		autoPlay: false,
+	});
 
 	const playAudio = async (): Promise<void> => {
 		setAyahStatus({ currentAyah: ayahNumber, currentSurah: surahNumber, isPlaying: true, lastAyah: ayahNumber - 1 > 0 ? ayahNumber - 1 : 1 });
-		await playOneAyah(surahNumber, ayahNumber);
+		// await playOneAyah(surahNumber, ayahNumber);
+		await controls.play();
 		setAyahStatus({ currentAyah: ayahNumber, currentSurah: surahNumber, isPlaying: false, lastAyah: ayahNumber - 1 > 0 ? ayahNumber - 1 : 1 });
 	};
+
+	useEffect(() => {
+		const ayahRecitation = async (): Promise<void> => {
+			const url = await getAyahRecitationUrl(surahNumber, ayahNumber);
+			setAyahUrl(url);
+		};
+		ayahRecitation().catch((err) => console.log(err));
+	}, []);
 
 	return (
 		<Popover>
@@ -29,7 +42,13 @@ export const AyahContainer = React.memo(({ lafz, ayahNumber, surahNumber }: { la
 			<PopoverContent dir='rtl' p={2}>
 				<PopoverArrow />
 				<PopoverBody dir='rtl'>
-					<Button onClick={() => playAudio()}>Play</Button>
+					{audio}
+
+					<ButtonGroup>
+						<Button onClick={() => playAudio()}>Play</Button>
+						<Button onClick={controls.pause}>Pause</Button>
+					</ButtonGroup>
+
 					<p>Ayah number - {ayahNumber}</p>
 					<p>Surah number - {surahNumber}</p>
 				</PopoverBody>
--- apps/tanzeel/src/components/Surah/QuranLafz/AyahDisplay.tsx
+++ apps/tanzeel/src/components/Surah/QuranLafz/AyahDisplay.tsx
@@ -2,11 +2,8 @@ import { Wrap } from '@chakra-ui/react';
 import { Lafz } from '../../../utils/types';
 import { AyahContainer } from './AyahContainer';
 
-export const AyahDisplay = ({ ayahs, surahNumber }: { ayahs: Lafz[][]; surahNumber: number; }): JSX.Element => {
-	console.log('surahNumber: ', surahNumber);
-	return (
-		<Wrap dir='rtl' spacing={7} width={800} flexDir='row'>
-			{ayahs.map((ayah, index) => <AyahContainer ayahNumber={index + 1} lafz={ayah} key={`${index + 1}`} surahNumber={surahNumber} />)}
-		</Wrap>
-	);
-};
+export const AyahDisplay = ({ ayahs, surahNumber }: { ayahs: Lafz[][]; surahNumber: number; }): JSX.Element => (
+	<Wrap dir='rtl' spacing={7} width={800} flexDir='row'>
+		{ayahs.map((ayah, index) => <AyahContainer ayahNumber={index + 1} lafz={ayah} key={`${index + 1}`} surahNumber={surahNumber} />)}
+	</Wrap>
+);
